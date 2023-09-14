91a194d7bce106e8ac9df8dcc9aedfba3865aac6
Steven Schweiger <sschweig90@gmail.com>
2023-09-14 15:28:52 -0400

    Implemented search, fixed CORS

--- app.py
+++ app.py
@@ -10,12 +10,32 @@ import typing
 import urllib
 from fastapi import FastAPI
 from dotenv import load_dotenv
+#from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
+#.env handling
 load_dotenv()
-
 BOOKS_API_KEY = os.getenv('BOOKS_API_KEY')
 
-app = FastAPI()
+origins = [
+    "*",
+    "localhost", 
+    "localhost:3000", 
+    "http://localhost", 
+    "http://localhost:3000"
+    "https://localhost", 
+    "https://localhost:3000",
+    "http://0.0.0.0",
+    "http://0.0.0.0/results/*",
+    "http://localhost:80"
+]
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins)
+]
+
+app = FastAPI(middleware=middleware)
+
 
 def search_url_builder(keywords: str) -> str:
     """Build URL for search functionality.
@@ -38,8 +58,11 @@ def search_url_builder(keywords: str) -> str:
         https://developers.google.com/books/docs/v1/using#PerformingSearch
 
     """
-    
     base = "https://www.googleapis.com/books/v1/volumes?"
+    
+    if len(keywords) == 0:
+        return base
+    
     return base + urllib.parse.urlencode({
         "q": keywords.replace(" ", "+")
         })
@@ -48,8 +71,20 @@ def search_url_builder(keywords: str) -> str:
 async def root() -> dict:
     return {"message": "Hello World"}
 
-@app.get("/search/{keywords}")
-async def search(keywords: str) -> dict:
+@app.get("/results/{keywords}")
+def search(keywords):
     params = {"key":BOOKS_API_KEY}
-    r = requests.get(search_url_builder(keywords), params=params)
-    return r
+    url = search_url_builder(keywords)
+    r = requests.get(url, params=params)
+    if r.status_code == 200:
+        filtered = [{
+            'uid': idx,
+            'book_id': el['id'],
+            'title': el['volumeInfo'].get('title', ''),
+            'authors': el['volumeInfo'].get('authors', []),
+            'imagePreview': el['volumeInfo']['imageLinks'].get('smallThumbnail', ''),
+            'description': el['searchInfo'].get('textSnippet', '')
+            } for idx, el in enumerate(r.json()['items'])]
+            
+        return {'books': filtered}
+    return r.json()
